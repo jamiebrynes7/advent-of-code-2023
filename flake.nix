b640063dbb70a7044cdbb5800a8ab5d4bd81b5e9
Jamie Brynes <jamiebrynes7@gmail.com>
2023-12-02 21:44:18 +0000

    add aoc cli for downloading problems

--- flake.nix
+++ flake.nix
@@ -27,11 +27,43 @@
           toolchain = pkgs.rust-bin.stable.latest.default.override {
             extensions = [ "rust-analyzer" "rust-src" ];
           };
+
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+
+          aoc-cli = rustPlatform.buildRustPackage rec {
+            pname = "aoc-cli";
+            version = "90c86580efd50cc45f971563ecd555fcf8e689f1";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "tobias-walle";
+              repo = "advent-of-code-cli";
+              rev = version;
+              hash = "sha256-YozoYVXlBa7x0mBhs0Z2CImX47Yx5bltLzluM/UehdM=";
+            };
+
+            # Need to include Apple's build dependencies only if we are on a darwin system
+            buildInputs = if pkgs.stdenv.isDarwin then
+              with pkgs.darwin.apple_sdk; [
+                frameworks.CoreFoundation
+                frameworks.CoreServices
+                frameworks.SystemConfiguration
+              ]
+            else
+              [ ];
+
+            cargoHash = "sha256-vSn24Stru0WjCBCkDd4oQnm65N2qTCcHVsX2nAZnV5Q=";
+
+            doCheck = false;
+          };
+
         in {
           default = devenv.lib.mkShell {
             inherit inputs pkgs;
             modules = [{
-              packages = [ toolchain ];
+              packages = [ toolchain aoc-cli ];
               env.RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
             }];
           };
